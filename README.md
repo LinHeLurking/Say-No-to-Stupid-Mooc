55c28b2d4e49ff15997a350367fe8007bf294a2f
chenyanfan <chenyanfan18@mails.ucas.ac.cn>
2023-03-30 01:39:02 +0800

    fix: cannot jump pdf reading; doc: give a solution to malfunction of auto click

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Say No to Stupid Mooc
 
 我只能说懂的都懂，不懂的也不好多解释什么。
+
+Important: If it does not work, it is perhaps because Chrome diables autoplay of videos. If you do not know how to solve it, I recommend you switch to Firefox and enables autoplay in the setting.
--- good_bye_mooc.user.js
+++ good_bye_mooc.user.js
@@ -6,7 +6,6 @@
 // @author       LinHe
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.4.0/jquery.min.js
 // @match        http://mooc1.ecourse.ucas.ac.cn/mycourse/*
-// @icon         https://www.google.com/s2/favicons?domain=ucas.ac.cn
 // @grant        none
 // ==/UserScript==
 
@@ -14,35 +13,6 @@
 
 (function () {
     'use strict';
-
-    // var t2 = window.setInterval(function() {
-
-    //     var eles = document.getElementsByClassName('vjs-big-play-button');
-    //     var btn = eles[0];
-    //     if(btn.hidden == false){
-    //         btn.click();
-    //     }
-
-
-
-    //  },100)
-
-    //  var tstop = window.setTimeout(function() {
-
-    //     var stop = document.getElementsByClassName("vjs-play-control vjs-control vjs-button vjs-playing")[0];
-    //     stop.onclick = function(){
-    //          window.clearInterval(t2);
-    //         console.log('stop');
-    //         var start = document.getElementsByClassName("vjs-play-control vjs-control vjs-button vjs-paused")[0];
-    //         start.onclick = function(){
-    //             window.setInterval(t2);
-    //         };
-    //     };
-    //  },1000)
-
-    // while($("#ext-gen1045").style["visibility"] != "hidden"){
-    //     $("#ext-gen1045").click();
-    // };
 
     const replayVideo = function () {
         const playButton = $("#video > div.vjs-control-bar > button.vjs-play-control.vjs-control.vjs-button.vjs-paused", $("iframe").contents().find("iframe").contents()[0])[0];
@@ -66,13 +36,11 @@
                             const video = frames[0];
                             const docs = frames.slice(1);
                             $(".vjs-big-play-button", video).click();
-                            window.setInterval(replayVideo, 100);
+                            window.setInterval(replayVideo, 200);
                             for (let index = 0; index < docs.length; index++) {
                                 const doc = docs[index];
-                                const rightButton = $(".nextBtn", doc)[0];
-                                while (rightButton.style["visibility"] != "hidden") {
-                                    rightButton.click();
-                                }
+                                const mask = $("#maskLayer", doc);
+                                mask.click();
                             }
                         } else {
                             console.log("ERROR: cannot find video and document");
@@ -83,6 +51,7 @@
                             console.log("Checking if it's finished.");
                             var flag = true;
                             const icons = $(".ans-attach-ct", $("iframe").contents());
+                            console.log(icons);
                             for (let index = 0; index < icons.length; index++) {
                                 const icon = icons[index];
                                 flag = flag && icon.className.includes("finished");
@@ -91,7 +60,7 @@
                                 console.log("It's finished.");
                                 break;
                             }
-                            await new Promise(r => setTimeout(r, 30000));
+                            await new Promise(r => setTimeout(r, 15000));
                         }
                         window.clearInterval(replayVideo);
                     }
